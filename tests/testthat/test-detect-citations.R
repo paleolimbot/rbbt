d431505a2ad6f08d896b5bbe59d8b32ddf07015a
Dmytro Perepolkin <dperepolkin@gmail.com>
2020-06-03 16:55:02 +0200

    add test for different Zotero key pattern

--- tests/testthat/test-detect-citations.R
+++ tests/testthat/test-detect-citations.R
@@ -1,22 +1,22 @@
 
 test_that("regex for citations works", {
   expect_identical(
-    bbt_detect_citations_chr("\n@citation1 [@citation2] but not \\@citation3"),
-    c("citation1", "citation2")
+    bbt_detect_citations_chr("\n@citation1 \n@citation1991AlphaBetaGamma [@citation2] [@citation2020author] but not \\@citation3 and not not \\@citation2019NotValid"),
+    c("citation1","citation1991AlphaBetaGamma", "citation2", "citation2020author")
   )
 })
 
 test_that("detect_citations can operate on a character vector or file", {
   expect_identical(
-    bbt_detect_citations("\n@citation1 [@citation2] but not \\@citation3"),
-    c("citation1", "citation2")
+    bbt_detect_citations("\n@citation1 \n@citation1991AlphaBetaGamma [@citation2] [@citation2020author] but not \\@citation3 and not not \\@citation2019NotValid"),
+    c("citation1","citation1991AlphaBetaGamma", "citation2", "citation2020author")
   )
 
   file <- tempfile()
-  write("\n@citation1 [@citation2] but not \\@citation3", file)
+  write("\n@citation1 \n@citation1991AlphaBetaGamma [@citation2] [@citation2020author] but not \\@citation3 and not not \\@citation2019NotValid", file)
   expect_identical(
     bbt_detect_citations(file),
-    c("citation1", "citation2")
+    c("citation1","citation1991AlphaBetaGamma", "citation2", "citation2020author")
   )
   unlink(file)
 })
