22969ee29c01067f7fd57a63f21caf99f0718e17
Robin Lovelace <rob00x@gmail.com>
2021-07-31 01:55:22 +0100

    Add test test

---
+++ tests/testthat/test-update.R
@@ -0,0 +1,24 @@
+
+test_that("bbt_update_bib function works", {
+  bib_file <- tempfile(fileext = ".bib")
+  mock_content <- sprintf('
+
+---
+bibliography: %s
+---
+
+
+This file contains a reference to @dunnington_etal18.
+
+', bib_file)
+
+  rmd_file <- tempfile(fileext = ".Rmd")
+  readr::write_file(mock_content, rmd_file)
+
+  bbt_update_bib(rmd_file, bib_file)
+
+  expect_match(readr::read_file(bib_file), "dunnington_etal18")
+
+  unlink(c(rmd_file, bib_file))
+})
+
