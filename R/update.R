a1d5a352539bbd1a8810ac4cbbea9693fc2d5fc2
Robin Lovelace <rob00x@gmail.com>
2021-07-31 01:59:59 +0100

    Inherit params

--- R/update.R
+++ R/update.R
@@ -4,14 +4,19 @@
 #' This function takes an RMarkdown file location and a bibliography file
 #' as inputs and updates the bibliography file.
 #'
+#' It also takes named arguments that can be passed to [bbt_write_bib()].
+#'
 #' @param path_rmd The path to the RMarkdown file.
 #' @param path_bib The path to the bibliography file.
+#' @inheritParams bbt_write_bib
 #'
 #' @return The location of the bibliography file that has been updated
 #' @export
-bbt_update_bib <- function(path_rmd, path_bib) {
+bbt_update_bib <- function(path_rmd, path_bib, ...) {
   bbt_write_bib(
     path = path_bib,
     keys = bbt_detect_citations(path_rmd),
-    overwrite = TRUE)
+    overwrite = TRUE,
+    ...
+    )
 }
